bda72e61c7d518e518ce31c3ddd12db6c8b6c12c
Brunomcdesa <brunocdesa.dev@gmail.com>
2026-03-24 19:35:13 -0300

    Primeiro commit

---
+++ src/main/java/org/metricfit/com/br/metricfit/MetricFitApplication.java
@@ -0,0 +1,13 @@
+package org.metricfit.com.br.metricfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/metricfit/com/br/metricfit/MetricFitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.metricfit.com.br.metricfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
